4be8801d6d25e0a9aeb5113a4e51022160dda497
Suko-Ribeiro <marcos.ribeiro@engenharia.ufjf.br>
2021-03-30 20:31:14 -0300

    Formatação e criação de variáveis da página de doação

--- FrontEnd/my-app/src/Components/Doacao/PaginaDoacao.jsx
+++ FrontEnd/my-app/src/Components/Doacao/PaginaDoacao.jsx
@@ -1,12 +1,24 @@
 import React, {Component} from "react";
 import "./Style.css";
-import CartaoAluno from "./Aluno";
-import Item from "./ListaItens";
-import LocalEntrega from "./LocalDeEntrega";
-import Detalhes from "./DetalhesDoacao";
+import avatar from "./avatar-crianca.png";
 
 
 class PaginaDoacao extends Component{
+    constructor(){
+        super();
+        this.nomeEscola = "Escola Estadual Plínio Barreto";
+        this.enderecoEscola = "Patati patatá";
+        this.listaMateriais = ["Lápis","Borracha","Caderno","Mochila"];
+        this.listaValor = ["1","2","10","20"]
+        this.listaDoados = [""];
+    }
+
+    handlerAddItem(){
+        console.log("teste")
+        var text = document.getElementById('item-a-doar')
+        this.listaDoados.push(text);
+    }
+    
     render(){
         return(
             <div>
@@ -15,22 +27,65 @@ class PaginaDoacao extends Component{
                         <h1>Faça sua Doação!</h1>
                     </header>
                     
-                    <CartaoAluno/>
+                    <div className="container">
+                        <div className="card mb-3">
+                            <div className="row no-gutters">
+                                <div className="col-sm-5">
+                                    <img src={avatar} alt="ilustração de menina ruiva"/>
+                                </div>
+                                <div className="col-sm-7">
+                                    <div className="card-body text-center">
+                                    <h5 className="card-title" id="nome-crianca">Maria</h5>
+                                    <p className="card-text" id="nome-escola">{this.nomeEscola}</p>
+                                    <p className="card-text">9<sup>o</sup> ano</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
                     <h4 className="subtitulo-formulario">Lista de Materiais</h4>
 
                     <form className="formulario-doacao">
                         <div>
-                            <Item/>
+                            <ul className="lista-itens">
+                                <li>
+                                    <p>Material     Doado</p>
+                                </li>
+                                    {this.listaMateriais.map((categoria,index) => {
+                                        return (
+                                        <li key={index}>
+                                            <div>
+                                                <label htmlFor="check" id="item-a-doar">{categoria}</label>
+                                                <input type="checkbox" id="check-doacao" onChange={this.handlerAddItem.bind(this)}/>
+                                            </div>
+                                        </li>
+                                    )
+                            })}</ul>
                         </div>
                         <h4 className="subtitulo-formulario">Ponto de Entrega</h4>
+                        <hr/>
                         <div>
-                            <LocalEntrega/>
-                        </div>
+                            <p className="titulo-pequeno" id="nome-escola">{this.nomeEscola}</p>
+                            <p>{this.enderecoEscola}</p>
+                        </div>  
                         <h4 className="subtitulo-formulario">Detalhes da Doação</h4>
+                        <hr/>
                         <div>
-                            <Detalhes/>
+                            <p className="titulo-pequeno">Nome do Doador</p>
+                            <input type="text" placeholder="Digite seu nome" id="nome-doador"/>
+                            <p className="titulo-pequeno">Itens a serem doados</p>
+                            <ul>
+                                {this.listaDoados.map((categoria,index) => {
+                                return (
+                                <li key={index}>
+                                    <p>{categoria}</p>
+                                </li>
+                                )})}
+                            </ul>
+                            <input type="checkbox" id="doacao-anonima"/>
+                            <label htmlFor="doacao-anonima">Doação anônima</label>
                         </div>
-                        <button class="botao btn btn-primary " type="submit" onsubmit="">
+                        <button className="botao btn btn-primary " type="submit" onSubmit="">
                             Confirmar Doação
                         </button>
 
