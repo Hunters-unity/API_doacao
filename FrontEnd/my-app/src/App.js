b92bef96cb605b408fcfba225019e843dc4f82bf
Suko-Ribeiro <marcos.ribeiro@engenharia.ufjf.br>
2021-04-03 11:15:16 -0300

    Responsividade Cartão Doação

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./reset.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Rotas from "./Routes";
+import CardAluno from './Components/Aluno/CardAluno';
 
 function App() {
   return (
--- FrontEnd/my-app/src/Components/Aluno/CardAluno.jsx
+++ FrontEnd/my-app/src/Components/Aluno/CardAluno.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./Style.css";
 import axios from 'axios'
-import { Container } from "reactstrap";
 import avatar from './avatar-crianca.png'
 
 class CardAluno extends Component {
@@ -10,38 +9,37 @@ class CardAluno extends Component {
     }
     render() {
         return (
-            <Container>
-                <div className="card mb-7">
-                    <div className="row">
-                        <div className="col">
-                            <img src={avatar} alt="ilustração - avatar aluno" className="col-img" />
-                        </div>
-                        <div className="col">
-                            <div className="card-body text-center">
-                                <h5 className="card-title" id="nome-responsável">Sônia Souza</h5>
-                                <p className="card-text" id="nome-escola">Escola Estadual Djalma</p>
-                                <p className="card-text">Escolaridade do Aluno: 9<sup>o</sup> ano</p>
-                            </div>
-                        </div>
-                        <div className="col-md">
-                            <form>
-                                <fieldset className="materiais-doados">
-                                    <legend className="">Lista de Materiais</legend>
-                                    <p><label><input type="checkbox" />Mochila</label></p>
-                                    <p><label><input type="checkbox" />Estojo</label></p>
-                                    <p><label><input type="checkbox" />Caderno</label></p>
-                                    <p><label><input type="checkbox" />Canetas</label></p>
-                                </fieldset>
-                            </form>
-                        </div>
-                        <div className="col">
-                            <a className="botao-doacao"><button className="btn btn-warning">Doar Agora</button></a>
-                        </div>
+            <div className="container card">
+                <div className="row">
+                    <div className="col">
+                        <img src={avatar} alt="ilustração - avatar aluno" className="col img" />
+                    </div>
+                    <div className="col titulo">
+                        <h5 className="card-title cartaoaluno" id="responsável">Responsável:</h5>
+                        <p className="card-text" id="nome-responsável">Sônia Souza</p>
+                        <h5 className="card-title cartaoaluno" id="escola">Escola:</h5>
+                        <p className="card-text" id="nome-escola">Escola Estadual Djalma</p>
 
                     </div>
-                </div>
+                    <div className="col md cartaoaluno">
+                        <form>
+                            <fieldset className="materiais-doados">
+                                <legend className="card-title cartaoaluno">Lista de Materiais</legend>
+                                <ul>
+                                    <li><input type="checkbox" id="Caneta" className="checkitem"/><label htmlFor="Caneta"> Caneta</label></li>
+                                    <li><input type="checkbox" id="Estojo" className="checkitem"/><label htmlFor="Estojo"> Estojo</label></li>
+                                    <li><input type="checkbox" id="Caderno" className="checkitem"/><label htmlFor="Caderno"> Caderno</label></li>
+                                    <li><input type="checkbox" id="Mochila" className="checkitem"/><label htmlFor="Mochila"> Mochila</label></li>
+                                </ul>
+                            </fieldset>
+                        </form>
+                    </div>
+                    <div className="col botao">
+                        <a className="botao-doacao"><button className="btn btn-warning">Doar Agora</button></a>
+                    </div>
 
-            </Container>
+                </div>
+            </div>
         )
     }
 }
